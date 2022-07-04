776c546063de8cf2fa815dc4801b4d77fe095d80
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2022-07-05 00:09:57 +0200

    build.js. Use  helper._getChecksum(), helper.copy()

--- build.js
+++ build.js
@@ -12,7 +12,7 @@ const replaceXml = require(`${pathBuildKram}/build/replaceXml.js`);
 const helper = require(`${pathBuildKram}/build/helper.js`);
 
 const pc = require(`${pathBuildKram}/node_modules/picocolors`);
-const fse = require(`${pathBuildKram}/node_modules/fs-extra`);
+// const fse = require(`${pathBuildKram}/node_modules/fs-extra`);
 
 let replaceXmlOptions = {};
 let zipOptions = {};
@@ -36,22 +36,13 @@ const pathMedia = `./media`;
 	];
 	await helper.cleanOut(cleanOuts);
 
-	console.log(pc.cyan(pc.bold(`Be patient! Some copy actions!`)));
-	await fse.copy(`${pathMedia}`, "./package/media"
-	).then(
-		answer => console.log(pc.yellow(pc.bold(
-			`Copied "${pathMedia}" to "./package/media".`))
-		)
-	);
+	from = pathMedia;
+	to = `./package/media`;
+	await helper.copy(from, to)
 
 	from = `./src`;
 	to = `./package`;
-	await fse.copy(from, to
-	).then(
-		answer => console.log(
-			pc.yellow(pc.bold(`Copied "${from}" into "${to}".`))
-		)
-	);
+	await helper.copy(from, to)
 
 	await helper.mkdir('./dist');
 
@@ -65,10 +56,9 @@ const pathMedia = `./media`;
 	};
 
 	await replaceXml.main(replaceXmlOptions);
-	await fse.copy(`${Manifest}`, `./dist/${manifestFileName}`).then(
-		answer => console.log(pc.yellow(pc.bold(
-			`Copied "${manifestFileName}" to "./dist".`)))
-	);
+	from = Manifest;
+	to = `./dist/${manifestFileName}`;
+	await helper.copy(from, to)
 
 	// Create zip file and detect checksum then.
 	const zipFilePath = path.resolve(`./dist/${zipFilename}`);
@@ -79,35 +69,14 @@ const pathMedia = `./media`;
 	};
 	await helper.zip(zipOptions)
 
-	const Digest = 'sha256'; //sha384, sha512
-	const checksum = await helper.getChecksum(zipFilePath, Digest)
-  .then(
-		hash => {
-			const tag = `<${Digest}>${hash}</${Digest}>`;
-			console.log(pc.green(pc.bold(`Checksum tag is: ${tag}`)));
-			return tag;
-		}
-	)
-	.catch(error => {
-		console.log(error);
-		console.log(pc.red(pc.bold(
-			`Error while checksum creation. I won't set one!`)));
-		return '';
-	});
-
-	replaceXmlOptions.checksum = checksum;
+	replaceXmlOptions.checksum = await helper._getChecksum(zipFilePath);
 
 	// Bei diesen werden zuerst Vorlagen nach dist/ kopiert und dort erst "replaced".
 	for (const file of [updateXml, changelogXml, releaseTxt])
 	{
 		from = file;
 		to = `./dist/${path.win32.basename(file)}`;
-		await fse.copy(from, to
-		).then(
-			answer => console.log(
-				pc.yellow(pc.bold(`Copied "${from}" to "${to}".`))
-			)
-		);
+		await helper.copy(from, to)
 
 		replaceXmlOptions.xmlFile = path.resolve(to);
 		await replaceXml.main(replaceXmlOptions);
